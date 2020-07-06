a3402e55063c34a0c3e8f55adeb4ef8690c09abb
KConch <67806937+KConch@users.noreply.github.com>
2020-07-06 12:43:30 -0500

    minor scene tests and changes

--- PlantGame/Assets/Scripts/SceneChanges.cs
+++ PlantGame/Assets/Scripts/SceneChanges.cs
@@ -1,18 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneChanges : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public void NewScene()
     {
-        
+        SceneManager.LoadScene("GreenHouse");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
