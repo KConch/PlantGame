6720ff583311a5e2dc7e484781d1587f9eb62940
KConch <67806937+KConch@users.noreply.github.com>
2020-07-06 16:42:12 -0500

    add simple player movement

---
+++ PlantGame/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rigbod;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigbod = GetComponent<Rigidbody2D>();
+
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rigbod.AddForce(movement * speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
